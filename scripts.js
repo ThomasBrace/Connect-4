1959c21fbfffba3be8a34ad99630ae48e1e8fa08
Thomas Brace <thomas.idris.brace@googlemail.com>
2018-10-03 10:27:59 +0100

    complete

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function Counter(color,x,y,index,playIndex){
     this.index = index,
     this.x = x,
     this.y = y,
-    this.playOrder = playIndex   
+    this.playOrder = playIndex
 }
 function Cell(x,y,index,content){
     this.index = index,
@@ -27,7 +27,7 @@ var game = {
     cells: [],
     totalX: 7,
     totalY: 7,
-    //setup function to iterate turns and switch player. 
+    //setup function to iterate turns and switch player.
     nextPlayer: function(currentPlayer){
         if (this.currentPlayer == 0){
             this.currentPlayer = 1;
@@ -39,7 +39,7 @@ var game = {
     //setup function to build game
     buildGrid: function() {
         var grid = "<div class='row'>";
-        for (var i=0;i <= game.cells.length-1; i++){         
+        for (var i=0;i <= game.cells.length-1; i++){
             if (game.cells[i].x == game.totalX){
                 grid += "<div onclick='game.dropCounter("+game.cells[i].index+")' class='cell' id=cell_" + game.cells[i].index +"></div></div><div class='row'>"
             } else {
@@ -48,37 +48,37 @@ var game = {
         };
         grid = grid.slice(0,grid.lastIndexOf("<div class='row'>")); //remove last unneeded row
         document.getElementById("container").innerHTML = grid; //render grid to container
-        
-        
-        
+
+
+
     },
     buildPlayers: function() {
         game.players.push(new Player("yellow"));
         game.players.push(new Player("red"));
     },
     //update score
-    score: function() { 
-        document.getElementById("player_one_turn").innerHTML =+ game.players[0].index; 
-        document.getElementById("player_one_longest").innerHTML =+ game.players[0].maxLength; 
-        
-        document.getElementById("player_two_turn").innerHTML =+ game.players[1].index; 
+    score: function() {
+        document.getElementById("player_one_turn").innerHTML =+ game.players[0].index;
+        document.getElementById("player_one_longest").innerHTML =+ game.players[0].maxLength;
+
+        document.getElementById("player_two_turn").innerHTML =+ game.players[1].index;
         document.getElementById("player_two_longest").innerHTML =+ game.players[1].maxLength;
     },
     //setup function to drop counter
     dropCounter: function(index){
         if (game.cells[index-1].content == null) {
-            
+
             var animationArray = []
 
             //drop counter untill out of bounds or hit existing counter
             while (game.cells[index-1] != undefined && game.cells[index-1].content == null){
                 document.getElementById("cell_"+index).className = document.getElementById("cell_"+index).className.replace("yellow_animate","");
-                document.getElementById("cell_"+index).className = document.getElementById("cell_"+index).className.replace("red_animate",""); 
+                document.getElementById("cell_"+index).className = document.getElementById("cell_"+index).className.replace("red_animate","");
                 index = index + game.totalX;
-                animationArray.push(index); 
+                animationArray.push(index);
             }
-            index = index - game.totalX; //revert back up last empty space. 
-            animationArray.splice(-2,2); //remove last animation frame. 
+            index = index - game.totalX; //revert back up last empty space.
+            animationArray.splice(-2,2); //remove last animation frame.
             //animationArray.reverse();
             for(var i=0;i<animationArray.length;i++){
                 if(game.players[game.currentPlayer].color == "yellow"){
@@ -102,10 +102,10 @@ var game = {
         }
     },
     winTest : function(obj){
-        
+
         var transitions = [[game.totalX,"vert"],[1,"horr"],[game.totalX+1,"diag1"],[game.totalX-1,"diag2"]]
-   
-   
+
+
         for(var i=0; i<transitions.length; i++){
             var orgObj = obj;
             //check verticle
@@ -114,39 +114,39 @@ var game = {
             currentLine.push(currentObj) //add current position to line
             //check + direction
             while (this.checkAdjacent(currentObj,transitions[i][0]-1,obj.color,transitions[i][1]) != false){
-                currentLine.push(this.checkAdjacent(currentObj,transitions[i][0]-1,obj.color,transitions[i][1])); 
-                currentObj = this.checkAdjacent(currentObj,transitions[i][0]-1,obj.color,transitions[i][1]);  
+                currentLine.push(this.checkAdjacent(currentObj,transitions[i][0]-1,obj.color,transitions[i][1]));
+                currentObj = this.checkAdjacent(currentObj,transitions[i][0]-1,obj.color,transitions[i][1]);
             }
             //reset to orignal counter & check - direction
             currentObj = orgObj
             while (this.checkAdjacent(currentObj,-transitions[i][0]-1,obj.color,transitions[i][1]) != false){
-                currentLine.push(this.checkAdjacent(currentObj,-transitions[i][0]-1,obj.color,transitions[i][1])); 
-                currentObj = this.checkAdjacent(currentObj,-transitions[i][0]-1,obj.color,transitions[i][1]);  
+                currentLine.push(this.checkAdjacent(currentObj,-transitions[i][0]-1,obj.color,transitions[i][1]));
+                currentObj = this.checkAdjacent(currentObj,-transitions[i][0]-1,obj.color,transitions[i][1]);
             }
-            
+
             //update maxLength
             if(currentLine.length > game.players[game.currentPlayer].maxLength){
                 game.players[game.currentPlayer].maxLength = currentLine.length;
             }
-            
+
             //test for win
             if (currentLine.length >= 4){
                 for(var i = 0; i < currentLine.length; i++){
                     document.getElementById("cell_"+currentLine[i].index).className += " "+game.players[game.currentPlayer].color + "_win";
                 }
                 alert(game.players[game.currentPlayer].color + " Wins!")
-            }    
-            
+            }
+
         }
     },
     checkAdjacent : function(currentObj,transition,color,dir){
         var i = currentObj.index+transition;
         var newObj = game.cells[i];
-        if (dir == "vert"){    
+        if (dir == "vert"){
             if(newObj !== undefined){ //test if beyond game area
                 if (newObj.content != null){ //test if has counter
                     if (newObj.content.color == color){ //test counter colour
-                        return game.cells[i].content;    
+                        return game.cells[i].content;
                     }
                 }
             }
@@ -155,7 +155,7 @@ var game = {
                 if (newObj.content != null){ //test if has counter
                     if (newObj.content.y === currentObj.y) { // test horrizontal
                         if (game.cells[i].content.color == color){ //test counter colour
-                            return game.cells[i].content;    
+                            return game.cells[i].content;
                         }
                     }
                 }
@@ -165,14 +165,14 @@ var game = {
                 if (newObj.content != null){ //test if has counter
                     if (newObj.content.y != currentObj.y) { /// test diag in not wrapping
                         if (game.cells[i].content.color == color){ //test counter colour
-                            return game.cells[i].content;    
+                            return game.cells[i].content;
                         }
                     }
                 }
             }
         }
         return false;
-    } 
+    }
 }
 
 //create cell objetcs.
